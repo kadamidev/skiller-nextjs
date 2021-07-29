fba044e6110891f7bfc217211012abec0d0d7234
Dev <kadamidev@users.noreply.github.com>
2021-07-29 22:05:30 +0700

    implemented applet menu page, complete on all viewports

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import Link from 'next/link'
+import styles from '../styles/menu/menu.module.scss'
+import Image from 'next/image'
+
 
 const menu = () => {
     return (
         <>
-            menu page
+            <div className={styles.container}>
+                <ul>
+                    <li className={styles.tasker}>
+                        <Link href='tasker_app'>
+                            <a>
+                                <div className={styles.svgCont}>
+                                    <Image src='/img/menu/tasker.svg' width={94} height={74} layout='responsive'/>
+                                </div>
+                                <span>Tasker</span>
+                            </a>
+                        </Link>
+                    </li>
+                    <li className={styles.none}><Link href='#'><a><span>Coming Soon</span></a></Link></li>
+                    <li className={styles.none}><Link href='#'><a><span>Coming Soon</span></a></Link></li>
+                    <li className={styles.none}><Link href='#'><a><span>Coming Soon</span></a></Link></li>
+                    <li className={styles.none}><Link href='#'><a><span>Coming Soon</span></a></Link></li>
+                    <li className={styles.none}><Link href='#'><a><span>Coming Soon</span></a></Link></li>
+
+                </ul>
+            </div>
         </>
     );
 }
