412d240e414e0d9550ca5bb9f843ccc7194cffde
Dev <kadamidev@users.noreply.github.com>
2021-07-31 21:01:20 +0700

    added card body structure

--- components/tasker_app/Card.jsx
+++ components/tasker_app/Card.jsx
@@ -3,6 +3,16 @@ import styles from '../../styles/app/Card.module.scss'
 import Image from 'next/image'
 
 const Card = () => {
+
+    const cardItems = [
+        { checked: false, text: 'New item'},
+        { checked: true, text: 'New item 2'},
+        { checked: true, text: 'New item 3'},
+        { checked: false, text: 'New item 4'},
+
+
+    ]
+
     return (
         <>
             <div className={styles.cardContainer}>
@@ -12,7 +22,22 @@ const Card = () => {
                     <div className={styles.collapseBtn}><Image src='/img/app/minus.svg' height={16} width={16} /></div>
                 </header>
                 <body>
-                    
+                    <ul>
+                        { cardItems.map(item => {
+                            return (
+                                <li>
+                                    <div className={styles.checkBox}>
+                                        { item.checked ?
+                                        <Image src='/img/app/checked.svg' width={16} height={16}/>
+                                        :
+                                        <Image src='/img/app/unchecked.svg' width={16} height={16}/>
+                                        }
+                                    </div>
+                                    <p className={styles.itemText}>{item.text}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </body>
             </div>
         </>
