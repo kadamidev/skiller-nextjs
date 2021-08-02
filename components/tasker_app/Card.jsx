7960aff1377eee8c180400da719c1a83db6eed4e
Dev <kadamidev@users.noreply.github.com>
2021-08-02 20:03:25 +0700

    added card collapsing functionality

--- components/tasker_app/Card.jsx
+++ components/tasker_app/Card.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/app/Card.module.scss'
 import Image from 'next/image'
 import CheckBox from './CheckBox';
 
-const Card = () => {
+
+const Card = (props) => {
+
+    const [collapsed, setCollapsed] = useState(props.collapsed)
 
     const cardItems = [
         { checked: false, text: 'New item'},
         { checked: true, text: 'New item 2'},
         { checked: true, text: 'New item 3'},
         { checked: false, text: 'New item 4'},
-
-
     ]
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed)
+    }
 
     return (
         <>
-            <div className={styles.cardContainer}>
+            <div className={!collapsed ? styles.cardContainer : [styles.cardContainer, styles.collapsed].join(" ")}>
                 <header>
                     <span>New Card</span>
                     <div className={styles.edit}><Image src='/img/app/edit.svg' height={16} width={16} /></div>
-                    <div className={styles.collapseBtn}><Image src='/img/app/minus.svg' height={16} width={16} /></div>
+                    <div className={styles.collapseBtn} onClick={toggleCollapsed}> <Image src={!collapsed ? '/img/app/minus.svg': '/img/app/plus.svg'} height={16} width={16} /> </div>
                 </header>
                 <section className={styles.cardBody}>
                     <ul>
