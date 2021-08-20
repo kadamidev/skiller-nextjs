a77660d428245e56bd2f5e1ff635821172114985
Dev <kadamidev@users.noreply.github.com>
2021-08-20 23:22:44 +0700

    added delete/edit tab buttons for desktop tab navbar

--- components/tasker_app/TabNav.jsx
+++ components/tasker_app/TabNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from '../../styles/app/TabNav.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -6,9 +6,10 @@ import TabsMenu from './TabsMenu';
 import { useMediaQuery } from '../../lib/useMediaQuery';
 
 const TabNav = ({darkMode, tabsState, dispatch}) => {
-
+    
     const [tabEdit, setTabEdit] = useState(false)
-
+    const [editableTabIdx, setEditableTabIdx] = useState(false)
+    const editNode = useRef(null)
     
     const toggleTabEdit = () => { setTabEdit(!tabEdit) }
     
@@ -18,13 +19,14 @@ const TabNav = ({darkMode, tabsState, dispatch}) => {
     const currentTab = tabsState.tabs[tabsState.currentTabIdx]
 
     const isDesktop = useMediaQuery('(min-width: 769px)')
+
     
     return (
         <>
         <nav className={styles.container}>
             {/* ------------------------- mobile --------------------------- */}
             { !isDesktop && <>
-            <div className={styles.homeWrap}>w
+            <div className={styles.homeWrap}>
                 <Link href='/menu'><a><Image src="/img/app/home.svg" width={30} height={30}/></a></Link>
             </div>  
             <div className={styles.tab}>
@@ -47,8 +49,29 @@ const TabNav = ({darkMode, tabsState, dispatch}) => {
             <ul className={styles.tabsList}>
                 {tabsState.tabs.map( (tab, index) => {
                     return (
-                    <li key={tab.id} className={(tabsState.currentTabIdx == index) ? [styles.desktopTab, styles.desktopCurrent].join(" ") : styles.desktopTab } onClick={ () => dispatch({type: 'changeCurrentTab', payload: {id: tab.id, idx: index} }) }>
+                    <li key={tab.id} className={(tabsState.currentTabIdx == index) ? [styles.desktopTab, styles.desktopCurrent].join(" ") : styles.desktopTab } onClick={ () => dispatch({type: 'changeCurrentTab', payload: {id: tab.id, idx: index} }) } >
+                        { (tabEdit && editableTabIdx == index) ?
+                        <input ref={editNode} className={styles.tabInput} type="text" value={tab.name} onChange={ (event) => dispatch({type: 'changeTabName', payload: { tabId: tab.id, name: event.target.value} }) } onBlur={toggleTabEdit} onClick={(e) => {e.stopPropagation()}}/>
+                        :
                         <span>{tab.name}</span>
+                        }
+                        <div className={styles.tabButtons}>
+                            <div className={styles.deleteTabWrapper} onClick={(e) => {
+                                e.stopPropagation()
+                                dispatch({ type: 'deleteTab', payload: {id: tab.id, idx: index} })
+                            }}>
+                                <Image src='/img/app/circled-x.svg' height={20} width={20}  />
+                            </div>
+                            <div className={styles.edit}>
+                                <Image src='/img/app/edit.svg' height={20} width={20} onClick={(e) => {
+                                    e.stopPropagation()
+                                    toggleTabEdit()
+                                    setEditableTabIdx(index)
+                                    console.log(editNode)
+                                    // editNode.current.focus()
+                                }} />
+                            </div>
+                        </div>
                     </li>
                     )
                 })}
