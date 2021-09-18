dae3530198253a9f1bf30b62bcdc1060036d3d1e
Dev <kadamidev@users.noreply.github.com>
2021-09-18 19:46:38 +0700

    fixed some alignment issues in homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac45f12f2870fe2607f1"; }
+/******/ 		__webpack_require__.h = function() { return "2cbb60bd05f9bb56a41c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
